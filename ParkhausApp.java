277481861edbdfcc8c86d4ce4bca361cadb3c1b1
Rinor Hagmann <rinorhagmann@outlook.com>
2025-08-16 18:05:50 +0200

    Grundstruktur angelegt

---
+++ ParkhausApp.java
@@ -0,0 +1,26 @@
+
+public class ParkhausApp {
+    public static void main(String[] args) {
+    
+    }
+}
+
+class Parkhaus {
+    
+}
+
+class Ticket {
+    
+}
+
+class Zahlstation {
+    
+}
+
+class Schranke {
+    
+}
+
+class Anzeigetafel {
+    
+}
